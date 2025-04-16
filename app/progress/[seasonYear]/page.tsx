c77923407aeb2bfe2473b232080062611b534e7c
bishtk435 <bishtk435@gmail.com>
2025-04-16 13:07:17 +0530

    width size of points table and team graph element fixed

--- app/progress/[seasonYear]/page.tsx
+++ app/progress/[seasonYear]/page.tsx
@@ -256,9 +256,9 @@ export default function PointsTableProgression() {
             </div>
             
             {/* Points Table and Graph side by side */}
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-5">
               {/* Points Table */}
-              <div className="order-2 lg:order-1">
+              <div className="order-2 lg:order-1 lg:col-span-7">
                 <PointsTable 
                   pointsTable={currentSnapshot.pointsTable}
                   selectedTeamId={selectedTeamId}
@@ -269,7 +269,7 @@ export default function PointsTableProgression() {
               </div>
               
               {/* Team Rank Graph */}
-              <div className="order-1 lg:order-2">
+              <div className="order-1 lg:order-2 lg:col-span-5">
                 <TeamRankGraph 
                   progression={pointsData.progression}
                   teamId={selectedTeamId}
